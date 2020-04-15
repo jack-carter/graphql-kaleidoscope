7dda68b1c67837a00b9893d454e5c320416059f5
Jack Carter <jack.carter@shinobigroup.com>
2020-04-15 16:51:28 -0500

    using-graphql version is now running successfully

--- using-graphql/schema.js
+++ using-graphql/schema.js
@@ -3,7 +3,7 @@
 
 const {
     GraphQLSchema,
-    GraphQLEnumType,
+    GraphQLNonNull,
     GraphQLObjectType,
     GraphQLBoolean,
     GraphQLString,
@@ -11,71 +11,26 @@ const {
 } = require('graphql')
 
 const {
-    Departments,
-    NO_MATCHES
+    Employees,
 } = require('../mock-dataloaders')
-
-function RESOLVING(item) {
-    console.log('Resolving "%s" ...',item)
-}
-
-const DepartmentCategoryType = new GraphQLEnumType({
-    name: 'DepartmentCategory',
-    values: {
-        ACCOUNTING: { value: 0 },
-        MARKETING: { value: 1 },
-        CUSTOMERSERVICE: { value: 2 }
-    }
-})
-
-const DepartmentType = new GraphQLObjectType({
-    name: 'Department',
-    fields: () => ({
-        // No resolvers necessary, as it will use the corresponding property.
-        type: { type: DepartmentCategoryType },
-        name: { type: GraphQLString },
-        
-        // Must have a resolver to narrow the list of Employees to just this department. 
-        employees: { 
-            type: GraphQLList(EmployeeType),
-            async resolve(parent,args) {
-                RESOLVING('Department.employees')
-                // TODO pull the Employees for this Department
-                return await NO_MATCHES // dummy return for now
-            } 
-        }
-    })
-})
 
 const EmployeeType = new GraphQLObjectType({
     name: 'Employee',
     fields: () => ({
         // No resolvers necessary, as it directly use the corresponding property.
-        name: { type: GraphQLString },
-        department: { type: DepartmentType },
-        fulltime: { type: GraphQLBoolean }
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        department: { type: new GraphQLNonNull(GraphQLString) },
+        fulltime: { type: new GraphQLNonNull(GraphQLBoolean) }
     })
 })
 
 const QueryType = new GraphQLObjectType({
     name: 'Query',
     fields: () => ({
-        departments: {
-            type: new GraphQLList(DepartmentType),
-            async resolve(parent,args) {
-                RESOLVING('departments ...')
-                let employees = await Departments.loadAll()
-                RESOLVING('departments: %s',departments)
-                return departments
-            }
-        },
         employees: {
-            type: new GraphQLList(EmployeeType),
+            type: new GraphQLNonNull( new GraphQLList( new GraphQLNonNull( EmployeeType )) ),
             async resolve(parent, args) {
-                RESOLVING('employees ...')
-                let employees = await Employees.loadAll()
-                RESOLVING('employees: %s',employees)
-                return employees
+                return await Employees.loadAll()
             }
         }
     })
